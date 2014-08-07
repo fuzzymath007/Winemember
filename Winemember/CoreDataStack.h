b8a293547dce0ca1a6a4797152f4074aaa823b4f
fuzzymath007 <mchess@umich.edu>
2014-08-06 22:51:09 -0400

    Moved core data properties and methods to its own class and out of the app delegate.

---
+++ Winemember/CoreDataStack.h
@@ -0,0 +1,20 @@
+//
+//  CoreDataStack.h
+//  Winemember
+//
+//  Created by Matthew Chess on 8/6/14.
+//  Copyright (c) 2014 matthewchess. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CoreDataStack : NSObject
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
